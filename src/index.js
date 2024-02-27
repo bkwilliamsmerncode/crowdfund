9c1c4e6205f5d1a564c785c34194e3f99966dbe9
Brian Williams <152632135+bkwilliamsmerncode@users.noreply.github.com>
2024-02-27 10:11:19 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter
+} from "react-router-dom"
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL} >
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 
